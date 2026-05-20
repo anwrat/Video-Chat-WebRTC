e038076b8ef303b7e2d52758c5059dbe66ba10af
anwrat <anweshrwt22@gmail.com>
2026-05-20 15:44:23 +0545

    Setting up a socket server in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,20 @@
 import config from './config/dotenv.config.js';
 import app from './app.js';
+import http from 'http';
+import {Server} from 'socket.io';
+import cors from 'cors';
 
-app.listen(config.port,()=>{
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket)=>{
+    console.log(`Connected to socket ${socket.id}`)
+});
+
+server.listen(config.port,()=>{
     console.log(`Server is running on port ${config.port}`);
 })
